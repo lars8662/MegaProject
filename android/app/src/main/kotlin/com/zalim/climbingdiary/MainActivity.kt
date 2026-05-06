6d5a3ae452b5c30682da5b3309516de8f43ff514
lars8662 <66337090+lars8662@users.noreply.github.com>
2026-05-06 18:48:18 +0300

    Keep screen awake during timer sessions

--- android/app/src/main/kotlin/com/zalim/climbingdiary/MainActivity.kt
+++ android/app/src/main/kotlin/com/zalim/climbingdiary/MainActivity.kt
@@ -1,5 +1,12 @@
 package com.zalim.climbingdiary
 
+import android.os.Bundle
+import android.view.WindowManager
 import io.flutter.embedding.android.FlutterActivity
 
-class MainActivity : FlutterActivity()
+class MainActivity : FlutterActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        window.addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON)
+    }
+}
